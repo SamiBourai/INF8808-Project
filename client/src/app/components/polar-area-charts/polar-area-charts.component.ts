3cd052d03ecfa23e2a8886c004284e6e81f851fb
sinda <sinda.drira@polymtl.ca>
2023-06-20 00:48:45 -0400

    added all 3 polar charts

--- client/src/app/components/polar-area-charts/polar-area-charts.component.ts
+++ client/src/app/components/polar-area-charts/polar-area-charts.component.ts
@@ -14,6 +14,12 @@ export interface DataType {
   color: string;
 }
 
+export interface DataDetails {
+  title: string;
+  scale: number;
+  tooltipTitle: string;
+}
+
 @Component({
   selector: 'app-polar-area-charts',
   templateUrl: './polar-area-charts.component.html',
@@ -24,19 +30,54 @@ export class PolarAreaChartsComponent implements OnInit, AfterViewInit {
   constructor() { } 
 
   private observer: IntersectionObserver | null = null;
+  private dataSets: DataType[][] = [];
   private data: DataType[] = [];
+  private currentDatasetIndex: number = 0;
+  private dataDetails: DataDetails[] = [];
+
 
   ngOnInit(): void {
 
-    this.data = [
-      { label: 'Morroco', value: 78.6, color: '#e80284' },
-      { label: 'Argentina', value: 84.6, color: '#4517EE' },
-      { label: 'France', value: 82.6, color: '#4517EE' },
-      { label: 'Croatia', value: 83.3 , color: '#4517EE' },
-      { label: 'Senegal', value: 77.7 , color: '#DB8500' },
-      { label: 'Tunisia', value: 73.8 , color: '#DB8500' },
-      { label: 'Ghana', value: 77.4 , color: '#DB8500' },
+    this.dataSets = [
+      [
+        { label: 'Morroco', value: 78.6, color: '#e80284' },
+        { label: 'Argentina', value: 84.6, color: '#4517EE' },
+        { label: 'France', value: 82.6, color: '#4517EE' },
+        { label: 'Croatia', value: 83.3 , color: '#4517EE' },
+        { label: 'Senegal', value: 77.7 , color: '#DB8500' },
+        { label: 'Tunisia', value: 73.8 , color: '#DB8500' },
+        { label: 'Ghana', value: 77.4 , color: '#DB8500' },
+      ],
+
+      [
+        { label: 'Morroco', value: 27.9, color: '#e80284' },
+        { label: 'Argentina', value: 43.2, color: '#4517EE' },
+        { label: 'France', value: 33.0, color: '#4517EE' },
+        { label: 'Croatia', value: 32.9 , color: '#4517EE' },
+        { label: 'Senegal', value: 23.5 , color: '#DB8500' },
+        { label: 'Tunisia', value: 25.0 , color: '#DB8500' },
+        { label: 'Ghana', value: 36.0 , color: '#DB8500' },
+      ],
+
+      [
+        { label: 'Morroco', value: 60, color: '#e80284' },
+        { label: 'Argentina', value: 60, color: '#4517EE' },
+        { label: 'France', value: 55.2, color: '#4517EE' },
+        { label: 'Croatia', value: 57.1 , color: '#4517EE' },
+        { label: 'Senegal', value: 55.6 , color: '#DB8500' },
+        { label: 'Tunisia', value: 50 , color: '#DB8500' },
+        { label: 'Ghana', value: 55.6 , color: '#DB8500' },
+      ]
+
     ];
+
+    this.dataDetails = [
+      {title:"% of Successful Passes", scale:70, tooltipTitle:"% of successful passes"},
+      {title:"% of Successful Shots", scale:20, tooltipTitle:"% of successful shots"},
+      {title:"% of Successful Goal Occasions", scale:45, tooltipTitle:"% of successful goal occasions"}
+    ];
+
+    this.data = this.dataSets[this.currentDatasetIndex]
     
   }
 
@@ -76,10 +117,12 @@ export class PolarAreaChartsComponent implements OnInit, AfterViewInit {
     const Max = (d3.max(this.data, function(d) { return d.value; }));
     
     const radiusScale = d3.scaleLinear()
-    .domain([70, Max as number])
+    .domain([this.dataDetails[this.currentDatasetIndex].scale, Max as number])
     .range([0, radius - 50]);
     
     const tooltip = d3.select('#tooltip');
+
+    const tooltipTitle = this.dataDetails[this.currentDatasetIndex].tooltipTitle;
 
     const svg = d3
       .select(element)
@@ -119,7 +162,7 @@ export class PolarAreaChartsComponent implements OnInit, AfterViewInit {
           .style('top', event.pageY - 75 + 'px').html(`
           <div>
           <div>${d.data.label}</div>
-          <div>% of Successful Passes</div>
+          <div>${tooltipTitle}</div>
           <div>${Math.abs(d.data.value)}</div>
           </div>
         `);
@@ -145,7 +188,7 @@ export class PolarAreaChartsComponent implements OnInit, AfterViewInit {
         const x = arc.centroid(d)[0];
         const y = arc.centroid(d)[1];
         const angle = Math.atan2(y, x) * (180 / Math.PI);
-        return `translate(${x}, ${y}) rotate(${angle}) translate(${radiusScale(d.value)/2 + 30}, 0) rotate(90)`;
+        return `translate(${x}, ${y}) rotate(${angle}) translate(${radiusScale(d.value)/2 + 43}, 0) rotate(90)`;
       })
       .attr("text-anchor", "middle")
       .attr("alignment-baseline", "middle")
@@ -163,7 +206,47 @@ export class PolarAreaChartsComponent implements OnInit, AfterViewInit {
       .attr('y', -225)
       .style("fill", '#dadad2')
       .style("font-size", "16px")
-      .text('% of succesful passes');
+      .text(this.dataDetails[this.currentDatasetIndex].title);
+
+    var legend = svg
+      .selectAll(".legend")
+      .data(['Morroco', 'Top 3', 'African countries'])
+      .enter()
+      .append("g")
+      .attr("class", "legend")
+      .attr("transform", (d, i) => `translate(${(-width/3) + i * 100} , ${200})`);
+    
+    legend
+      .append('rect')
+      .attr('x', 0)
+      .attr('width', 18)
+      .attr('height', 18)
+      .style('fill', function (d, i) {
+        return ['#e80284', '#4517EE', '#DB8500'][i];
+      });
+    
+    legend
+      .append('text')
+      .attr('x', 24)
+      .attr('y', 9)
+      .attr('dy', '.35em')
+      .style('text-anchor', 'start')
+      .style("fill", '#dadad2')
+      .text(d => d);
+  }
+
+  @HostListener('click', ['$event'])
+  updateChartData($event: Event) {
+    this.removeChart();
+
+    this.currentDatasetIndex++;
+    if (this.currentDatasetIndex >= this.dataSets.length) {
+      this.currentDatasetIndex = 0;
+    }
+
+    this.data = this.dataSets[this.currentDatasetIndex];
+
+    this.createChart();
   }
 
   removeChart() {
