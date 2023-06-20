f1aa05c16c026e9cbb07ad56ecce57c422e00ffb
theocouturieux <79099202+theocouturieux@users.noreply.github.com>
2023-06-19 21:06:55 -0400

    viz1-2 first commit

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; // Import it here
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,7 +25,11 @@ import { PossessionTopFourComponent } from './components/possession-top-four/pos
     ParallelCoordinatesChartComponent,
     PolarAreaChartsComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule,
+            AppRoutingModule,
+            BrowserAnimationsModule,
+            HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
